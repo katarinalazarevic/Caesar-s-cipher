63d744572095ed1ade71ebe338e4f5c98ecd9e56
LukaDj01 <lukaluxy01@gmail.com>
2024-12-07 16:43:02 +0100

    initial

--- BACKEND/obj/Debug/net7.0/WebTemplate.AssemblyInfo.cs
+++ BACKEND/obj/Debug/net7.0/WebTemplate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09fbaba98c23e1cbafc51783eee4da8d7ac26506")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
